0d19dc4450047f8129695c19d5c8cecd10b34eb3
Hyeonseo4799 <s21035@gsm.hs.kr>
2024-05-23 10:26:08 +0900

    :sparkles: :: 응답 값에 맞게 dto 수정

--- core/network/src/main/java/com/skogkatt/network/model/NetworkContentResponse.kt
+++ core/network/src/main/java/com/skogkatt/network/model/NetworkContentResponse.kt
@@ -5,7 +5,7 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class NetworkContentResponse(
-    @SerialName("response") val contents: List<NetworkContent>,
+    @SerialName("response") val content: NetworkContent,
 )
 
 @Serializable
