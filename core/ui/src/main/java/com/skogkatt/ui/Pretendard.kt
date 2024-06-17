46712b209d9a8947b837cd1d5a990050e243f2d7
Hyeonseo4799 <s21035@gsm.hs.kr>
2024-06-17 11:11:02 +0900

    :bento: :: pretendard font 추가

---
+++ core/ui/src/main/java/com/skogkatt/ui/Pretendard.kt
@@ -0,0 +1,12 @@
+package com.skogkatt.ui
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+
+val pretendard = FontFamily(
+    Font(R.font.pretendard_medium, FontWeight.Medium),
+    Font(R.font.pretendard_semibold, FontWeight.SemiBold),
+    Font(R.font.pretendard_bold, FontWeight.Bold)
+)
+
