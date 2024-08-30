0baeb8e5a85872a52992ff1b9ab0003f77265b3e
Hyeonseo4799 <s21035@gsm.hs.kr>
2024-08-30 10:21:49 +0900

    :sparkles: :: :core:media module 추가

---
+++ core/media/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    id("alio.android.library")
+    id("alio.android.hilt")
+}
+
+android {
+    namespace = "com.skogkatt.media"
+
+    defaultConfig {
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.media3.exoplayer)
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.ext)
+    androidTestImplementation(libs.androidx.test.espresso)
+}
