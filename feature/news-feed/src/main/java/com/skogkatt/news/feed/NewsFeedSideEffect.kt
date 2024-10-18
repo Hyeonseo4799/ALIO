a1f3f67456ab62a251cf89a5252d34933845492e
조현서 <s21035@gsm.hs.kr>
2024-10-18 15:55:07 +0900

    :green_heart: :: Spacing before ":"

--- feature/news-feed/src/main/java/com/skogkatt/news/feed/NewsFeedSideEffect.kt
+++ feature/news-feed/src/main/java/com/skogkatt/news/feed/NewsFeedSideEffect.kt
@@ -2,5 +2,5 @@ package com.skogkatt.news.feed
 
 sealed class NewsFeedSideEffect {
     data class ShowSnackbar(val error: String?) : NewsFeedSideEffect()
-    data class OnNewsClicked(val id: String): NewsFeedSideEffect()
+    data class OnNewsClicked(val id: String) : NewsFeedSideEffect()
 }
